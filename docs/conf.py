4b494bc236f307d6617825b7ba2c8dc6cb3f3467
Brian McFee <brian.mcfee@nyu.edu>
2015-10-21 11:33:17 -0400

    fixed parse error in sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../muda'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'numpydoc',
