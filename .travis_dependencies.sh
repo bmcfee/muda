4de3cf089b73c37731053b5d2ea7a90d4b44846b
Brian McFee <brian.mcfee@nyu.edu>
2015-10-20 21:43:05 -0400

    simplifying travis config

--- .travis_dependencies.sh
+++ .travis_dependencies.sh
@@ -24,33 +24,23 @@ if [ ! -f "$HOME/env/miniconda.sh" ]; then
         wget http://repo.continuum.io/miniconda/Miniconda-3.16.0-Linux-x86_64.sh -O miniconda.sh;
 
         # Install both environments
-        bash miniconda.sh -b -p $HOME/env/miniconda27
-        bash miniconda.sh -b -p $HOME/env/miniconda34
-        bash miniconda.sh -b -p $HOME/env/miniconda35
+        src="$HOME/env/miniconda$TRAVIS_PYTHON_VERSION"
+        bash miniconda.sh -b -p $src
 
-        for version in 2.7 3.4 3.5 ; do
-            if [[ "$version" == "2.7" ]]; then
-                src="$HOME/env/miniconda27"
-            elif [[ "$version" == "3.4" ]]; then
-                src="$HOME/env/miniconda34"
-            else
-                src="$HOME/env/miniconda35"
-            fi
-            OLDPATH=$PATH
-            export PATH="$src/bin:$PATH"
-            conda_create $version
+        OLDPATH=$PATH
+        export PATH="$src/bin:$PATH"
+        conda_create $version
 
-            source activate $ENV_NAME
+        source activate $ENV_NAME
 
-            pip install python-coveralls
-            pip install pysoundfile jsonpickle
-            pip install --no-deps mir_eval audioread decorator librosa pyrubberband
-            pip install --no-deps git+https://github.com/marl/jams.git
+        pip install python-coveralls
+        pip install pysoundfile jsonpickle
+        pip install --no-deps mir_eval audioread decorator librosa pyrubberband
+        pip install --no-deps git+https://github.com/marl/jams.git
 
-            source deactivate
+        source deactivate
 
-            export PATH=$OLDPATH
-        done
+        export PATH=$OLDPATH
     popd
 else
     echo "Using cached dependencies"
