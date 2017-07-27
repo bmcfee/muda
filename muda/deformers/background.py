7854d0d42180211203a019dc9376cd47dbf479ff
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-07-27 16:46:37 -0400

    Fixes #59: `BackgroundNoise` fails if `len(soundf)==n_target`
    If `len(soundf) < n_target`, raises a `RuntimeError` which prints the
    faulty values of `filename`, `len(soundf)` and `n_target`.
    
    This is a breaking change, because the maximal value for the random
    integer `start` is increased by 1.
    
    Milestone: v0.2

--- muda/deformers/background.py
+++ muda/deformers/background.py
@@ -35,12 +35,17 @@ def sample_clip_indices(filename, n_samples, sr):
     '''
 
     with psf.SoundFile(str(filename), mode='r') as soundf:
-
+        # Measure required length of fragment
         n_target = int(np.ceil(n_samples * soundf.samplerate / float(sr)))
 
-        # Draw a random clip
-        start = np.random.randint(0, len(soundf) - n_target)
-        stop = start + n_target
+        # Raise exception if source is too short
+        if len(soundf) < n_target:
+            raise RuntimeError('Source {} (length={})'.format(filename, len(soundf)) +
+                ' must be at least the length of the input ({})'.format(n_target))
+
+        # Draw a starting point at random in the background waveform
+        start = np.random.randint(0, 1 + len(soundf) - n_target)
+        stop = start + target_length
 
         return start, stop
 
